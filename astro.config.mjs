c267c1699dceca94e0078cb4943f0dbe0044c031
I̷r̷ <irharuki7@gmail.com>
2024-12-28 17:23:43 +0000

    aadapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server", // atau 'hybrid'
+  adapter: vercel(),
   vite: {
     define: {
       'import.meta.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
